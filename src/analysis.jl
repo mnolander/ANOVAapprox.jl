9863196555aaf714a36b1c6f1d198fd55e04c3a5
Franziska Nestler <franziska.nestler@mathematik.tu-chemnitz.de>
2022-07-04 10:46:38 +0200

    functions get_variances and get_ShapleyValues

--- src/analysis.jl
+++ src/analysis.jl
@@ -1,4 +1,60 @@
 @doc raw"""
+    get_variances( a::approx, λ::Float64; dict::Bool = false )::Union{Vector{Float64},Dict{Vector{Int},Float64}}
+
+This function returns the variances of the approximated ANOVA terms with ``\lambda`` as a vector for `dict = false` or else a dictionary.
+"""
+function get_variances(
+    a::approx,
+    λ::Float64;
+    dict::Bool = false,
+)::Union{Vector{Float64},Dict{Vector{Int},Float64}}
+    if a.basis == "wav1"
+        variances = norms(a.fc[λ], 1, dict = false) .^ 2
+    elseif a.basis == "wav2"
+        variances = norms(a.fc[λ], 2, dict = false) .^ 2
+    elseif a.basis == "wav3"
+        variances = norms(a.fc[λ], 3, dict = false) .^ 2
+    elseif a.basis == "wav4"
+        variances = norms(a.fc[λ], 4, dict = false) .^ 2
+    else
+        variances = norms(a.fc[λ]) .^ 2
+    end
+    variances = variances[2:end]
+
+    if dict
+        gsis = Dict{Vector{Int},Float64}()
+        if a.basis == "wav1"
+            variances = norms(a.fc[λ], 1, dict = true)
+        elseif a.basis == "wav2"
+            variances = norms(a.fc[λ], 2, dict = true)
+        elseif a.basis == "wav3"
+            variances = norms(a.fc[λ], 3, dict = true)
+        elseif a.basis == "wav4"
+            variances = norms(a.fc[λ], 4, dict = true)
+        else
+            variances = norms(a.fc[λ], dict = true)
+        end
+
+        return Dict((u, variances[u]) for u in keys(variances))
+
+    else
+        return variances
+    end
+end
+
+@doc raw"""
+    get_variances( a::approx; dict::Bool = false )::Dict{Float64,Union{Vector{Float64},Dict{Vector{Int},Float64}}}
+
+This function returns the variances of the approximated ANOVA terms for all ``\lambda`` as a vector for `dict = false` or else a dictionary.
+"""
+function get_variances(
+    a::approx;
+    dict::Bool = false,
+)::Dict{Float64,Union{Vector{Float64},Dict{Vector{Int},Float64}}}
+    return Dict(λ => get_variances(a, λ, dict = dict) for λ in collect(keys(a.fc)))
+end
+
+@doc raw"""
     get_GSI( a::approx, λ::Float64; dict::Bool = false )::Union{Vector{Float64},Dict{Vector{Int},Float64}}
 
 This function returns the global sensitivity indices of the approximation with ``\lambda`` as a vector for `dict = false` or else a dictionary.
@@ -138,4 +194,34 @@ end
 
 function get_ActiveSet(a::approx, eps::Vector{Float64})::Dict{Float64,Vector{Vector{Int}}}
     return Dict(λ => get_ActiveSet(a, eps, λ) for λ in collect(keys(a.fc)))
+end
+
+@doc raw"""
+    get_ShapleyValues( a::approx, λ::Float64 )::Vector{Float64}
+
+This function returns the Shapley values of the approximation for reg. parameter ``\lambda`` as a vector of length `a.d`.
+"""
+function get_ShapleyValues(a::approx, λ::Float64)::Vector{Float64}
+    d = size(a.X, 1)
+    vars = get_variances(a, λ, dict = true)
+    U = collect(keys(vars))
+
+    r = zeros(Float64, d)
+
+    for i = 1:d
+        for v in U
+            if (i in v)
+                r[i] += vars[v]^2 / length(v)
+            end
+        end
+    end
+
+    return r
+end
+
+@doc raw"""
+This function returns the Shapley values of the approximation for all reg. parameters ``\lambda`` as a dictionary of vectors of length `a.d`.
+"""
+function get_ShapleyValues(a::approx)::Dict{Float64,Vector{Float64}}
+    return Dict(λ => get_ShapleyValues(a, λ) for λ in collect(keys(a.fc)))
 end
