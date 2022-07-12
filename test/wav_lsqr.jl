8d2d47efdce24de55aca56e55abf758add2d1596
Laura Lippert <laura.lippert@mathematik.tu-chemnitz.de>
2022-07-12 16:29:54 +0200

    changed also notation in test file

--- test/wav_lsqr.jl
+++ test/wav_lsqr.jl
@@ -9,26 +9,26 @@ d = 6
 ds = 2
 M = 10_000
 max_iter = 50
-bw = [4, 4]
+bw = [4,4]
 λs = [0.0, 1.0]
 
 
-X = rand(d, M) .- 0.5
+X = rand( d, M) .- 0.5
 y = [TestFunctionPeriodic.f(X[:, i]) for i = 1:M]
 X_test = rand(d, M) .- 0.5
 y_test = [TestFunctionPeriodic.f(X_test[:, i]) for i = 1:M]
 
 ####  ####
 
-ads = ANOVAapprox.approx(X, y, ds, bw, "wav2")
+ads = ANOVAapprox.approx(X, y, ds, bw, "chui2")
 ANOVAapprox.approximate(ads, lambda = λs)
 
-println("AR: ", sum(ANOVAapprox.get_AttributeRanking(ads, 0.0)))
-@test abs(sum(ANOVAapprox.get_AttributeRanking(ads, 0.0)) - 1) < 0.0001
+println( "AR: ", sum(ANOVAapprox.get_AttributeRanking(ads, 0.0)) )
+@test abs( sum(ANOVAapprox.get_AttributeRanking(ads, 0.0)) - 1 ) < 0.0001
 
-bw = ANOVAapprox.get_orderDependentBW(TestFunctionPeriodic.AS, [4, 4])
+bw = ANOVAapprox.get_orderDependentBW(TestFunctionPeriodic.AS, [4,4])
 
-aU = ANOVAapprox.approx(X, y, TestFunctionPeriodic.AS, bw, "wav2")
+aU = ANOVAapprox.approx(X, y, TestFunctionPeriodic.AS, bw, "chui2")
 ANOVAapprox.approximate(aU, lambda = λs)
 
 err_l2_ds = ANOVAapprox.get_l2error(ads)[0.0]
